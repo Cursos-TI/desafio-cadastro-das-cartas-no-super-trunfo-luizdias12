73d04a47c85a6f90a8aa1dcff6a4a41348e627d4
Luiz Dias da Silva Junior <67482208+luizdias12@users.noreply.github.com>
2025-01-15 21:34:08 +0000

    commit inicial - definiçao das variaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,12 +7,27 @@
 //Teste larissa
 
 int main() {
+
+    print("Cadastro das cartas Super Trunfo\n");
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+
+    //Definindo as variaveis
+
+    char codigo_estado;
+    char codigo_cidade[2];
+    char codigo_carta[3];
+    int populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;
     
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+
+    // Entrada dos dados
     
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
